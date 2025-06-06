2dd853f27cb9f5a0439929032f06e2b13e07e9b0
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-06-06 23:26:16 +0300

    Enhance authentication flow by adding user creation logic and redirect handling in callback route

--- src/app/auth/v1/callback/route.ts
+++ src/app/auth/v1/callback/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -5,9 +6,33 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
-  if (code) {
-    const supabase = await createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+  if (!code) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  const supabase = await createClient();
+  await supabase.auth.exchangeCodeForSession(code);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email!,
+        name: user.user_metadata.name,
+        image: user.user_metadata.avatar_url,
+      },
+    });
   }
 
   return NextResponse.redirect(new URL("/dashboard", request.url));
